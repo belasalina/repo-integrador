9ff39286172c3ca9f02dc3ad88774caef905aaa6
belasalina <bela.salina@gmail.com>
2019-04-24 18:57:49 -0300

    registro

--- registro.php
+++ registro.php
@@ -18,9 +18,8 @@
    <head>
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-     <!-- Bootstrap CSS -->
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-     <link rel="stylesheet" href=C:\xampp\htdocs\bela\digitalhouse\tp-integrador\css\estilo-registro.css>
+     <link rel="stylesheet" href="C:\Users\user\Desktop\github\estilo-registro.css">
      <title>| Registrate </title>
    </head>
 
